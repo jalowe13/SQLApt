35b43ca32c23c1c758ea42d26aac5fa4bce42ce5
jalowe13 <jplowe21@vt.edu>
2024-01-30 22:09:20 -0600

    First Api Request to Apartment List
    Test API Call
    To test generate an api key with https://rapidapi.com/epctex-epctex-default/api/apartment-list
    And make a key.txt file in the root directory

---
+++ SQLApt/Keys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Api for Apartments.com
+// https://rapidapi.com/epctex-epctex-default/api/apartment-list
+
+namespace SQLApt
+{
+    internal static class Keys
+    {
+        public static string ApiKey = "";
+        public const string ApiHost = "apartment-list.p.rapidapi.com";
+
+        public static void GrabKey()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"..\..\key.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    ApiKey = line;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The key file could not be read or does not exist:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- SQLApt/Program.cs
+++ SQLApt/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +10,53 @@ namespace SQLApt
 {
     internal class Program
     {
+        // Start the Program
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!\n");
-            System.Console.ReadKey();
+            Type keysType = Type.GetType("SQLApt.Keys");
+            if (keysType == null)
+            {
+                Console.WriteLine("Keys class not found. Please ensure you have a Keys.cs class");
+                throw new InvalidOperationException("Keys class is not defined.");
+            }
+            Keys.GrabKey(); // Grab Key from the Key text file
+
+            string ApiKey = Keys.ApiKey;
+            string ApiHost = Keys.ApiHost;
+
+            Console.WriteLine("Your API Key is...");
+            Console.WriteLine(ApiKey);
+            Console.WriteLine("Your API Host is...");
+            Console.WriteLine(ApiHost);
+            Console.WriteLine("Calling the API...");
+            ApiCall().GetAwaiter().GetResult();
+            Console.WriteLine("Api Called.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        // Call the Api To Apartment.com
+        static async Task ApiCall()
+        {
+            // Create a New Http Client
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apartment-list.p.rapidapi.com/properties/p153763"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", Keys.ApiKey },
+                    { "X-RapidAPI-Host", Keys.ApiHost },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
         }
     }
 }
